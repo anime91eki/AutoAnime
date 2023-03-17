8908f1c724685b63ac52dd3c106a9c71c0035b30
Luffy <120936732+anime91eki@users.noreply.github.com>
2023-03-17 20:25:05 +0530

    Update __init__.py
    Signed-off-by: Luffy <120936732+anime91eki@users.noreply.github.com>

--- Bot/__init__.py
+++ Bot/__init__.py
@@ -15,8 +15,8 @@ headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:55.0)
 
 API_ID = int(environ.get('API_ID', 25654389)) #API ID
 API_HASH = environ.get('API_HASH', '89d5973b8325a5a800e515eabc8c6d5e') #API HASH
-BOT_TOKEN = environ.get('BOT_TOKEN', 'Your Bot Token') #BOT TOKEN
-DATABASE_URL = environ.get('DATABASE_URL', 'Your MongoDb') #MONGO DB
+BOT_TOKEN = environ.get('BOT_TOKEN', '6208725968:AAGy_FxD3lEzdFGUA_aKoUJPE45fEQg6L9Q') #BOT TOKEN
+DATABASE_URL = environ.get('DATABASE_URL', 'mongodb+srv://Abhi12:abhi12@cluster0.bdue3ap.mongodb.net/?retryWrites=true&w=majority') #MONGO DB
 OWNER_ID = int(environ.get('OWNER_ID', 5009250822)) #OWNER ID
 MAIN_CHANNEL = int(environ.get('MAIN_CHANNEL', -1001854781135))#YOUR MAIN CHANNEL ID
 ARCHIVE_CHANNEL = int(environ.get('ARCHIVE_CHANNEL',-1001863366316))#YOUR ARCHIVE CHANNEL
